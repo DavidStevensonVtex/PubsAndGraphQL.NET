68082d640b9184dff5a2d877ae1686cafe26b42f
David Stevenson <david.stevenson@vtex.com>
2019-10-30 12:16:46 -0400

    Initial commit
    Added Pubs data, GraphQL.NET Schema first and Graph Type first libraries.
    
    Added unit tests for both Schema first and Graph Type first libraries.

---
+++ PubsGraphTypeFirst/PublishersQuery.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+
+namespace Pubs.Types
+{
+    public class PublishersQuery : ObjectGraphType<PublisherType>
+    {
+        public PublishersQuery()
+        {
+            Name = "PublishersQuery";
+            Description = "Return array of Publishers";
+            Field<ListGraphType<PublisherType>>(
+                "publishers",
+                description: "All publishers",
+                resolve: _ => PubsData.Publishers);
+        }
+    }
+}
---
+++ PubsGraphTypeFirst/TitlesQuery.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pubs.Types
+{
+    public class TitlesQuery : ObjectGraphType<TitleType>
+    {
+        public TitlesQuery()
+        { 
+            Name = "AuthorsQuery";
+            Description = "Return array of Titles";
+            Field<ListGraphType<TitleType>>(
+                "titles",
+                description: "All titles",
+                resolve: _ => PubsData.Titles);
+        }
+    }
+}
